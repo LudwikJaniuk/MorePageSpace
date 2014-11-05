2f5f6ce1d98917cd49be98dc48f35fb422bf9dc5
LudwikJaniuk <ludv.jani-2015@vrg.se>
2014-11-05 22:03:28 +0100

    Create background.js

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    code: 'document.body.innerHTML = document.body.innerHTML + "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>"'
+  });
+});
